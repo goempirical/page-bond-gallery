f731950aa3da70c6448bde47f0f67e9f6d690763
Juan Cardenas <jcardenas@decodeit.co>
2018-04-13 16:30:12 -0500

    - Setup local environment with wordpress and understrap https://trello.com/c/Um4tQuEZ/35-2-setup-local-environment-with-wordpress-and-understrap-2 - New theme basic setup https://trello.com/c/Tfc8ZOtv/15-3-new-theme-basic-setup-1

---
+++ wp-content/themes/page-bond-gallery/functions.php
@@ -0,0 +1,60 @@
+<?php
+function understrap_remove_scripts() {
+    wp_dequeue_style( 'understrap-styles' );
+    wp_deregister_style( 'understrap-styles' );
+
+    wp_dequeue_script( 'understrap-scripts' );
+    wp_deregister_script( 'understrap-scripts' );
+
+    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
+}
+add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+
+	// Get the theme data
+	$the_theme = wp_get_theme();
+    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
+    wp_enqueue_script( 'jquery');
+	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
+    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+}
+
+function google_map_api() {
+    $api['key'] = 'AIzaSyBfUJ2B05OTiOmiPpaGLNK8_BaXeFXiATA';
+        return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'google_map_api');
+
+// Catching var query via URL
+
+function query_range_years( $vars ) {
+    $vars[] = 'range';
+    return $vars;
+}
+
+add_filter('query_vars', 'query_range_years');
+
+include 'custom-post-types/index.php';
+include 'taxonomies/index.php';
+
+// Add support to files upload SVG
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+add_filter('nav_menu_css_class' , 'nav_class_active' , 10 , 2);
+
+function nav_class_active ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
