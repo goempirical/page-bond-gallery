c997376f741c2c293ed06c570cd33197b76b75af
spencer8 <spencer@team-eight.com>
2019-01-09 18:47:35 -0500

    ups excerpt length and fixes news sidebar

--- wp-content/themes/page-bond-gallery/archive.php
+++ wp-content/themes/page-bond-gallery/archive.php
@@ -32,51 +32,48 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 				<div class="aux_content">
 					<div class="row justify-content-between">
+						<div class="col-md-8 col-xl-7 content-area" id="primary">
+							<main class="site-main" id="main">
+								<?php /* Start the Loop */ ?>
+								<?php while ( have_posts() ) : the_post(); ?>
 
+									<?php
 
-			<div class="col-md-8 col-xl-7 content-area" id="primary">
+									/*
+									 * Include the Post-Format-specific template for the content.
+									 * If you want to override this in a child theme, then include a file
+									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+									 */
+									get_template_part( 'loop-templates/content', 'news' );
+									?>
 
-			<main class="site-main" id="main">
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
+								<?php endwhile; ?>
 
+							<?php else : ?>
+
+								<div class="aux_content box-stroke adding__space adding__padding__top">
+									<div class="row">
+									<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+
+							<?php endif; ?>
+
+						</main><!-- #main -->
+
+						<div class="blogroll-footer d-flex justify-content-between"> 
 						<?php
+						previous_posts_link( '&lt; Previous' );
+						next_posts_link( 'Next &gt;' );
+					 	?>
+					 	</div>
+					</div><!-- #primary -->
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', 'news' );
-						?>
+					<!-- Do the right sidebar check -->
+					<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 
-					<?php endwhile; ?>
-
-				<?php else : ?>
-
-				<div class="aux_content box-stroke adding__space adding__padding__top">
-					<div class="row">
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
-
-				<?php endif; ?>
-
-			</main><!-- #main -->
-
-			<div class="blogroll-footer d-flex justify-content-between"> 
-			<?php
-			previous_posts_link( '&lt; Previous' );
-			next_posts_link( 'Next &gt;' );
-		 	?>
-		 	</div>
-		</div><!-- #primary -->
-
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-	</div> <!-- .row -->
-</div>
-</div>
-</div><!-- Container end -->
+				</div> <!-- .row -->
+			</div>
+		</div>
+	</div><!-- Container end -->
 
 </div><!-- Wrapper end -->
 
